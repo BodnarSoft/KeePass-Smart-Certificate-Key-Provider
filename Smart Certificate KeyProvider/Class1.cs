a627fe82b6e291fac6417dde9436526de6038adc
František Bodnár <frantisek.bodnar-ext@xitee.com>
2018-03-22 15:16:22 +0100

    Add project files.

---
+++ Smart Certificate KeyProvider/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Certificate_KeyProvider
+{
+    public class Class1
+    {
+    }
+}
